e4e31dbb3ddae3bb602fceeea28c5c80b0cc387a
voa2000 <voa2000@hotmail.com>
2019-02-06 11:42:41 +0000

    Renaming file

---
+++ src/OOPIntro.java
@@ -0,0 +1,15 @@
+
+public class OOPIntro {
+
+
+		public static void main(String[] args) {
+
+			SuperHero spiderman = new SuperHero("Spiderman","Wrist web-shooters to shoot spider web material",10);
+			SuperHero batman = new SuperHero("Batman","Night vision",10)
+	;
+
+			System.out.println(spiderman.getName()+ spiderman.getSuperpower()+spiderman.getsStrength());
+			System.out.println(batman.getName()+batman.getSuperpower()+" "+batman.getsStrength());
+		}
+
+	}
